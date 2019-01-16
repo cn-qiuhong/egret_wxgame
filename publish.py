dab3de4a867ee08731c69b8d798283286a779840
qiuhong <1195109313@qq.com>
2019-01-16 16:44:08 +0800

    增加打包js脚本publish.py，解决自定义组件命名空间的问题

---
+++ publish.py
@@ -0,0 +1,23 @@
+import os
+path = os.path
+cwd = os.getcwd()
+
+if __name__ == '__main__':
+    manifestFile = path.join(cwd, 'manifest.js')
+    text = ''
+    with open(manifestFile, 'r', encoding='UTF-8') as f:
+        txs = f.readlines()
+        for i in txs:
+            idx1 = i.find('"')
+            if idx1 < 0:
+                continue
+            idx2 = i.find('"', idx1+1)
+            if idx2 < 0:
+                continue
+            n = i[idx1+1:idx2]
+            fn = path.join(cwd, n)
+            with open(fn, 'r', encoding='UTF-8') as nf:
+                text += nf.read()
+    with open(manifestFile, 'w', encoding='UTF-8') as f:
+        f.write(text)
+    os.system('uglifyjs manifest.js -m -o manifest.js')
